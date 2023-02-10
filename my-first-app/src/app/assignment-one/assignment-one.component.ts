30e3e503b49fd0113ec8d18c26833b15dfcc0d5e
nken <nken@lle.rochester.edu>
2023-02-10 16:42:51 -0500

    notes on assignement

--- my-first-app/src/app/assignment-one/assignment-one.component.ts
+++ my-first-app/src/app/assignment-one/assignment-one.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-assignment-one',
+  selector: '[app-assignment-one]',
   templateUrl: './assignment-one.component.html',
   styleUrls: ['./assignment-one.component.css']
 })
