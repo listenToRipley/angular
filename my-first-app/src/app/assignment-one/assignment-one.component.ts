2ec1457de5d23123d9238bc2a9b65c4ec8e1b2b8
nken <nken@lle.rochester.edu>
2023-02-10 16:39:35 -0500

    create new component

---
+++ my-first-app/src/app/assignment-one/assignment-one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-one',
+  templateUrl: './assignment-one.component.html',
+  styleUrls: ['./assignment-one.component.css']
+})
+export class AssignmentOneComponent {
+
+}
