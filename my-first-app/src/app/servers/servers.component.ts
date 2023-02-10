8a76a0f9fd406a478b4807fee4ea874da967c66e
nken <nken@lle.rochester.edu>
2023-02-10 16:56:07 -0500

    style of warning

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -4,10 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',//use as a html tag
   // selector: '[app-servers]', //use as a attribute
   //selector: '.app-servers', //use as a class; id and pseudo selectors won't work with angular.
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `, //allows you to reference html within our ts file.
+  templateUrl: './servers.component.html',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`, //allows you to reference html within our ts file.
   //Inline code, backticks allow for js multiline formatting.
   styleUrls: ['./servers.component.css']
 })
