b477ecfb3a512dd806979197d3c4263d8219ab8a
nken <nken@lle.rochester.edu>
2023-03-24 15:58:41 -0400

    have issues with text and stay at 5th, all of them turn blue

--- assign3/src/app/solution/solution.component.ts
+++ assign3/src/app/solution/solution.component.ts
@@ -3,12 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-solution',
   templateUrl: './solution.component.html',
+  styles: [`
+    .fives {
+      color: white;
+    }
+  `]
 })
 export class SolutionComponent implements OnInit {
+
+  text: string = "Cauliflower cheese chalk and cheese say cheese. Stilton manchego edam fondue danish fontina croque monsieur emmental st. agur blue cheese. When the cheese comes out everybody's happy monterey jack the big cheese say cheese say cheese fondue taleggio who moved my cheese. Queso feta."
+  displayText: boolean = false;
+  clickLog:string[] = [];
+  background: string = "white";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDisplay() {
+    let time = `${new Date().getTime()}`
+
+    this.displayText = !this.displayText;
+
+    this.clickLog.push(time);
+
+  };
+
+  addBackgroundColor() {
+    return this.background = this.clickLog.length <= 5 ? 'white': 'blue';
   }
 
 }
